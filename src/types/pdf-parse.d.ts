a7ba6cc9cd17026273e314c43fbeea49a307c316
Lovepreet Singh <lovepreetsingh9810573475@gmail.com>
2026-05-07 13:24:02 +0530

    Update pdf-parse.d.ts

--- src/types/pdf-parse.d.ts
+++ src/types/pdf-parse.d.ts
@@ -1,12 +1,18 @@
+interface PdfParseResult {
+  numpages: number;
+  numrender: number;
+  info: unknown;
+  metadata: unknown;
+  text: string;
+  version: string;
+}
+
 declare module 'pdf-parse' {
-  interface PdfParseResult {
-    numpages: number;
-    numrender: number;
-    info: unknown;
-    metadata: unknown;
-    text: string;
-    version: string;
-  }
+  function pdfParse(buf: Buffer | Uint8Array, opts?: unknown): Promise<PdfParseResult>;
+  export default pdfParse;
+}
+
+declare module 'pdf-parse/lib/pdf-parse.js' {
   function pdfParse(buf: Buffer | Uint8Array, opts?: unknown): Promise<PdfParseResult>;
   export default pdfParse;
 }
